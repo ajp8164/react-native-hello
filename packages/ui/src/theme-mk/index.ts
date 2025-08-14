3ab55057a6f3617cf8ff822ca329f8a27ab3e821
Andy Phillipson <1063765+ajp8164@users.noreply.github.com>
2025-08-14 13:35:36 -0400

    Theme mk exports

--- packages/ui/src/theme-mk/index.ts
+++ packages/ui/src/theme-mk/index.ts
@@ -2,6 +2,10 @@ import { darkTheme } from './dark-theme';
 import { lightTheme } from './light-theme';
 import { ThemeManager as ThemeManagerCreator } from 'react-native-theme-mk';
 
+export * from './types';
+export { darkTheme } from './dark-theme';
+export { lightTheme } from './light-theme';
+
 export const ThemeManager = new ThemeManagerCreator(
   'light',
   {
