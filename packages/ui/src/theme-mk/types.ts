1a405e927ab96ec1c1c32d6ee7ede52f767fc381
Andy Phillipson <1063765+ajp8164@users.noreply.github.com>
2025-08-14 18:35:57 -0400

    Fix the base theme key values for editor autocomplete

--- packages/ui/src/theme-mk/types.ts
+++ packages/ui/src/theme-mk/types.ts
@@ -1,5 +1,17 @@
 import { type ColorValue } from 'react-native';
 import { type IStyle } from 'react-native-theme-mk';
+import type {
+  IColor,
+  IFonts,
+  IFontSizes,
+  ILineHeights,
+  IPalette,
+  IRadius,
+  IShadows,
+  ISpacings,
+  IStyles,
+  ITexts,
+} from 'theme-mk';
 
 export interface IThemeManagerTextStyle {
   color: ColorValue | string;
@@ -29,17 +41,17 @@ export type IShadow =
   | undefined;
 
 export interface IBaseThemeSchema {
-  radius: Record<string, number>;
-  colors: Record<string, ColorValue>;
-  fonts: Record<string, string>;
-  lineHeight: Record<string, number>;
-  spacing: Record<string, number>;
-  fontSize: Record<string, number>;
+  radius: Record<keyof IRadius, number>;
+  colors: Record<keyof IColor, ColorValue>;
+  fonts: Record<keyof IFonts, string>;
+  lineHeight: Record<keyof ILineHeights, number>;
+  spacing: Record<keyof ISpacings, number>;
+  fontSize: Record<keyof IFontSizes, number>;
 }
 
 export interface IThemeManagerSchema extends IBaseThemeSchema {
-  palette: Record<string, string>;
-  styles: Record<string, IStyle>;
-  text: Record<string, IThemeManagerTextStyle>;
-  shadow: Record<string, IShadow>;
+  palette: Record<keyof IPalette, string>;
+  styles: Record<keyof IStyles, IStyle>;
+  text: Record<keyof ITexts, IThemeManagerTextStyle>;
+  shadow: Record<keyof IShadows, IShadow>;
 }
